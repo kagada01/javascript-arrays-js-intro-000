7c927e8927db63798c6b3bb25806dd6480464a9a
Dan Kagan <dankagan7@gmail.com>
2019-03-02 19:40:45 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array) {
-  array.unshift("foo")
+ [...array, "foo"]
   return array
 }
